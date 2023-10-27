bc430d117e4a52ecc0fc253301dfb1e635da33ba
Valantis <siopis2008@hotmail.com>
2023-10-27 14:55:15 +0300

    Navigation-Table-homePage

--- Pantry/myapp/src/components/home.js
+++ Pantry/myapp/src/components/home.js
@@ -7,6 +7,11 @@ function Home() {
   return (
     <div className="body">
       <img className="img" src={PantryImg}></img>
+      <div className="pantryText">
+         <h1 className="welcome">WELCOME</h1>
+         <p> <span><Link to="/login" className="linkLogIn">Log in </Link></span>to track your Pantry</p>
+         <p> Or <span><Link to="/register" className="linkLogIn">Register</Link></span> to create your Pantry</p>
+      </div>
     </div>
   );
 }
